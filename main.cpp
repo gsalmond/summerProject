5a9a519a1d63cd69cd5661e9b768355ea541a3e0
Gray Salmond <graysalmond@hotmail.com>
2017-12-05 19:52:27 +1300

    Checking difference in files for simplified debugging purposes

--- main.cpp
+++ main.cpp
@@ -11,13 +11,17 @@ int main () {
     if(!inFile.is_open()) { cout<< "Error"; exit(EXIT_FAILURE); }
 
     char c;
+    outFile << "This is different" << endl;
     while(inFile.get(c)) {
         outFile << c;
         cout << c;
     }
 
+
     inFile.close();
     outFile.close();
+
+    system("diff ../main.cpp ../main.huffCode");
 
     test blah;
     cout << "Hello World" << endl;
