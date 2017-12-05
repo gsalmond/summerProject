f1fceaaa8bfa239c79ac6b6581f17b1cd34617ce
Gray Salmond <graysalmond@hotmail.com>
2017-12-05 19:42:29 +1300

    Reading in file, outputing same file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <fstream>
+
 #include "test.h"
 
 using namespace std;
 
 int main () {
+    ifstream inFile("../main.cpp");
+    ofstream outFile("../main.huffCode");
+    if(!inFile.is_open()) { cout<< "Error"; exit(EXIT_FAILURE); }
+
+    char c;
+    while(inFile.get(c)) {
+        outFile << c;
+        cout << c;
+    }
+
+    inFile.close();
+    outFile.close();
+
     test blah;
     cout << "Hello World" << endl;
     cout << blah.f();
