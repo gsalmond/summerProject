0bc89351c27362b535af49c4e9c4ac09cb2901eb
Gray Salmond <graysalmond@hotmail.com>
2017-12-06 21:10:25 +1300

    Adding some comments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+// 159.333 Programming Project Summer School 2017
+// File compressor based on Huffman's code
+// Author: Gray Salmond <graysalmond@hotmail.com>
+
 #include <iostream>
 #include <fstream>
 
@@ -6,10 +10,18 @@
 using namespace std;
 
 int main () {
-    ifstream inFile("../cantCorpus/alice29.txt");
-    ofstream outFile("../cantCorpus/alice29.huffCode");
+
+    //TODO eventually grab file(s) to compress from command line arguments
+    //     possibly extend to allow various compression algorithms and options
+    string inStr = "../cantCorpus/alice29.txt";
+    string outStr = "../cantCorpus/alice29.huffCode";
+
+    //TODO call Huffman's code compression passing file(s) to be compressed
+    ifstream inFile(inStr);
+    ofstream outFile(outStr);
     if(!inFile.is_open()) { cout<< "Error"; exit(EXIT_FAILURE); }
 
+    //TODO remove this segment
     char c;
     outFile << "This is different" << endl;
     while(inFile.get(c)) {
@@ -17,13 +29,14 @@ int main () {
         //cout << c;
     }
 
-
+    //TODO move file operations into compression related class
     inFile.close();
     outFile.close();
 
+    //TODO display statistics for compressed and decompressed file, perform check to see decoded file matches original
     system("diff ../cantCorpus/alice29.txt ../cantCorpus/alice29.huffCode");
 
+    //just testing multiple files
     test blah;
-    cout << "Hello World" << endl;
     cout << blah.f();
 }
